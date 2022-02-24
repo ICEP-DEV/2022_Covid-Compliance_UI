17e8e12c93f44644c05a249645f938337c1cd2b9
Hamilton Marobane <61621312+hmaputla@users.noreply.github.com>
2022-02-24 19:58:53 +0200

    login2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//import { HomeComponent } from './welcome/welcome.component';
+//import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './components/welcome/login/login.component';
+//import { RegisterComponent } from './register/register.component';
+import { RegisterComponent } from './components/welcome/register/register.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes =
+ [
+    {path:'',component:WelcomeComponent},
+    {path:'login',component:LoginComponent},
+    {path:'register',component:RegisterComponent}
+ ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LogoComponent } from './logo/logo.component';
-import { LoginComponent } from './login/login.component';
-import { FooterComponent } from './footer/footer.component';
+//import { LogoComponent } from './../logo/logo.component';
+import { LogoComponent } from './components/welcome/logo/logo.component';
+//import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './components/welcome/login/login.component';
+import { FooterComponent } from './components/welcome/footer/footer.component';
+//import { RegisterComponent } from './register/register.component';
+import { RegisterComponent } from './components/welcome/register/register.component';
+//import { IndexComponent } from './index/index.component';
+import { IndexComponent } from './components/welcome/index/index.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +20,7 @@ import { FooterComponent } from './footer/footer.component';
     LogoComponent,
     LoginComponent,
     FooterComponent,
+    RegisterComponent,IndexComponent, WelcomeComponent
   ],
   imports: [
     BrowserModule,
